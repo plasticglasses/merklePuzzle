ebaf4da211f2dfe24cec0e226517ca4ea161979a
plasticglasses <33756800+plasticglasses@users.noreply.github.com>
2020-11-23 16:58:42 +0000

    added PuzzleCracker to classpath

--- src/Merkle.java
+++ src/Merkle.java
@@ -1,8 +1,11 @@
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
@@ -12,57 +15,56 @@ public class Merkle {
 		PuzzleCreator alicesPuzzles = new PuzzleCreator();
 		alicesPuzzles.puzzleList = alicesPuzzles.createPuzzles();
 		alicesPuzzles.encryptPuzzlesToFile("puzzles.bin");
-		
-		//BOB crack a puzzles
-		PuzzleCracker bob = new PuzzleCracker();
+
+		PuzzleCracker bob = new PuzzleCracker("puzzles.bin");
 		Puzzle bobsPuzzle = bob.crack(34);
-		
+
 		SecretKey aKey = alicesPuzzles.findKey(34);
-		
+
 		try {
-			 
- 
-            Cipher ecipher = Cipher.getInstance("DES");
-            Cipher dcipher = Cipher.getInstance("DES");
- 
-            // initialize the ciphers with the given key
- 
-  ecipher.init(Cipher.ENCRYPT_MODE, aKey);
- 
-  dcipher.init(Cipher.DECRYPT_MODE, aKey);
- 
-  String message = "Testing Merkle Puzzles";
-  
-  byte[] messageBytes = message.getBytes("UTF8");
-	 
-byte[] enc = ecipher.doFinal(messageBytes);
+
+			Cipher ecipher = Cipher.getInstance("DES");
+			Cipher dcipher = Cipher.getInstance("DES");
+
+			// initialize the ciphers with the given key
+
+			ecipher.init(Cipher.ENCRYPT_MODE, aKey);
+
+			dcipher.init(Cipher.DECRYPT_MODE, aKey);
+
+			String message = "Testing Merkle Puzzles";
+
+			byte[] messageBytes = message.getBytes("UTF8");
+
+			byte[] enc = ecipher.doFinal(messageBytes);
 
 // encode to base64
 
-String encString = new String(Base64.getEncoder().encodeToString(enc));
+			String encString = new String(Base64.getEncoder().encodeToString(enc));
 
-System.out.println("Encryped message " + encString);
-bob.decryptMessage(enc);
- 
-        }
-        catch (NoSuchAlgorithmException e) {
-            System.out.println("No Such Algorithm:" + e.getMessage());
-            return;
-        }
-        catch (NoSuchPaddingException e) {
-            System.out.println("No Such Padding:" + e.getMessage());
-            return;
-        }
-        catch (InvalidKeyException e) {
-            System.out.println("Invalid Key:" + e.getMessage());
-            return;
-        }
-		
+			System.out.println("Encryped message " + encString);
+			bob.decryptMessage(enc);
+
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("No Such Algorithm:" + e.getMessage());
+			return;
+		} catch (NoSuchPaddingException e) {
+			System.out.println("No Such Padding:" + e.getMessage());
+			return;
+		} catch (InvalidKeyException e) {
+			System.out.println("Invalid Key:" + e.getMessage());
+			return;
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
-	
-	
-	
-	
-	
+
 }
